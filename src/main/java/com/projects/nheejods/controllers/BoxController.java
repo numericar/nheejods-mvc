264af2fa3911c09d875d4fb889cf7b5efbc07322
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-25 00:24:10 +0700

    add config

---
+++ src/main/java/com/projects/nheejods/controllers/BoxController.java
@@ -0,0 +1,16 @@
+package com.projects.nheejods.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class BoxController {
+
+    @GetMapping
+    public String viewBoxsPage() {
+        return "index";
+    }
+
+}
