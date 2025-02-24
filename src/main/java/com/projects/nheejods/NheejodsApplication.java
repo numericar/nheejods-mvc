4e3631483c8de30b68dfbb634bf97dd52e5eefa1
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-24 17:22:25 +0700

    initial project

---
+++ src/main/java/com/projects/nheejods/NheejodsApplication.java
@@ -0,0 +1,13 @@
+package com.projects.nheejods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NheejodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NheejodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/nheejods/NheejodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.nheejods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NheejodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
