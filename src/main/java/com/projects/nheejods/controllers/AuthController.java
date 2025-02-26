269292236ae48fb4bcb6cd6f5edba66372bac349
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-26 19:22:10 +0700

    add auths controller

---
+++ src/main/java/com/projects/nheejods/controllers/AuthController.java
@@ -0,0 +1,12 @@
+package com.projects.nheejods.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auths")
+public class AuthController {
+
+    
+
+}
